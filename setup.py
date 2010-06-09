d45dd421f4c512559b68ff1b1a0db28fbe298353
Alexander Artemenko <svetlyak.40wt@gmail.com>
2010-06-09 21:32:55 +0000

    Swith to the setuptools.

--- setup.py
+++ setup.py
@@ -8,24 +8,15 @@
 # which you should have received as part of this distribution.
 #
 
-# from ez_setup import use_setuptools
-from distutils.core import setup
-import sys
+try:
+    import setuptools
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
 
-# if 'cygwin' in sys.platform.lower():
-#     min_version = '0.6c6'
-# else:
-#     min_version = '0.6a9'
-# 
-# try:
-#     use_setuptools(min_version=min_version)
-# except TypeError:
-#     # locally installed ez_setup won't have min_version
-#     use_setuptools()
-# 
-# from setuptools import setup, find_packages, Extension, Feature
+from setuptools import setup, find_packages, Extension, Feature
 
-VERSION          = '0.2.3.1'
+VERSION          = '100609r1'
 DESCRIPTION      = 'SleekXMPP is an elegant Python library for XMPP (aka Jabber, Google Talk, etc).'
 LONG_DESCRIPTION = """
 SleekXMPP is an elegant Python library for XMPP (aka Jabber, Google Talk, etc).
@@ -37,18 +28,6 @@ CLASSIFIERS      = [ 'Intended Audience :: Developers',
                      'Topic :: Software Development :: Libraries :: Python Modules',
                    ]
 
-packages     = [ 'sleekxmpp', 
-				 'sleekxmpp/plugins',
-				 'sleekxmpp/stanza',
-				 'sleekxmpp/xmlstream',
-				 'sleekxmpp/xmlstream/matcher',
-				 'sleekxmpp/xmlstream/handler' ]
-
-if sys.version_info < (3, 0):
-	packages.append('sleekxmpp/xmlstream/tostring26')
-else:
-	packages.append('sleekxmpp/xmlstream/tostring')
-
 setup(
     name             = "sleekxmpp",
     version          = VERSION,
@@ -59,7 +38,7 @@ setup(
     url          = 'http://code.google.com/p/sleekxmpp',
     license      = 'MIT',
     platforms    = [ 'any' ],
-	packages	 = packages,
+    packages     = find_packages(),
     requires     = [ 'tlslite', 'pythondns' ],
     )
 
